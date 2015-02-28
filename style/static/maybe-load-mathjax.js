a62f4b478185d2d0f01c1fbaa40acbb62e5b6e75
Zack Weinberg <zackw@panix.com>
2015-02-27 23:35:43 -0500

    Remove MathJax again, it makes pelican take over a minute to compile.

--- style/static/maybe-load-mathjax.js
+++ style/static/maybe-load-mathjax.js
@@ -3,11 +3,17 @@
 
     function maybeLoadMathML () {
         var findMath = document.getElementsByTagName("math");
-        var script;
+        var cfg, script;
         if (findMath.length) {
+            cfg = document.createElement("script");
+            cfg.setAttribute("type", "text/x-mathjax-config");
+            cfg.appendChild(document.createTextNode(
+'MathJax.Hub.Config({extensions:["FontWarnings.js","MatchWebFonts.js"],"HTML-CSS":{preferredFont:"STIX",webFont:"STIX-Web",imageFont:null},FontWarnings:{webFont:null},MathMenu:{showLocale:false}})'));
+            document.body.appendChild(cfg);
+
             script = document.createElement("script");
             script.async = true;
-            script.src = "/s/MathJax/MathJax.js?config=MML_HTMLorMML.js";
+            script.src = "/s/MathJax.48b594af/MathJax.js?config=MML_HTMLorMML.js";
             document.body.appendChild(script);
         }
     }
